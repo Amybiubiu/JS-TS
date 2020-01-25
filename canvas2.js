62a914b04200b2229b1f922ea032cfb2b3a06c0f
Amybiubiu <1246457100@qq.com>
2020-01-25 20:04:25 +0800

    comp res

--- canvas2.js
+++ canvas2.js
@@ -4,23 +4,33 @@ const results = [
   {name: "Unsatisfied", count: 510, color: "pink"},
   {name: "No comment", count: 175, color: "silver"}
 ];
-  let cx = document.querySelector("canvas").getContext("2d");
-  let total = results
-    .reduce((sum, {count}) => sum + count, 0);
-  let currentAngle = -0.5 * Math.PI;
-  let centerX = 300, centerY = 150;
+let cx = document.querySelector("canvas").getContext("2d");
+let total = results
+  .reduce((sum, {count}) => sum + count, 0);
+let currentAngle = -0.5 * Math.PI;
+let centerX = 300, centerY = 150;
 
-  cx.font = "20px Georgia white";
-  // 在此循环中添加绘制切片标签的代码
-  for (let result of results) {
-    let sliceAngle = (result.count / total) * 2 * Math.PI;
-    cx.arc(centerX, centerY, 100,
-           currentAngle, currentAngle + sliceAngle);
-    currentAngle += sliceAngle;
-    cx.lineTo(centerX, centerY);
-    cx.fillStyle = result.color;
-    cx.fill();
-
-    cx.lineTO();
-    cx.fillText(result.name);
-  }
+// Add code to draw the slice labels in this loop.
+for (let result of results) {
+  let sliceAngle = (result.count / total) * 2 * Math.PI;
+  cx.beginPath();
+  cx.arc(centerX, centerY, 100,
+         currentAngle, currentAngle + sliceAngle);
+  
+  let arcMiddle = currentAngle + 0.5*sliceAngle;
+  cx.textBaseLine = "middle";
+  if(Math.cos(arcMiddle) > 0){
+      cx.textAlign = "left";
+  }else
+    cx.textAlign = "right";
+  let textX = Math.cos(arcMiddle)*130 + centerX;
+  let textY = Math.sin(arcMiddle)*130 + centerY;
+  cx.font = "15px sans-serif";
+  cx.fillStyle = "black";
+  cx.fillText(result.name,textX,textY);
+  
+  currentAngle += sliceAngle;
+  cx.lineTo(centerX, centerY);
+  cx.fillStyle = result.color;
+  cx.fill();
+}
